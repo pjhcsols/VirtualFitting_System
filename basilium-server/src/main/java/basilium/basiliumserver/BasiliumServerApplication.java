1f53ef869de671cd354fa5ac2222d02147b0a195
HanSol <110244523+pjhcsols@users.noreply.github.com>
2024-03-06 18:26:13 +0900

    Init : server 초기화 [박한솔]
    서버 초기화

---
+++ basilium-server/src/main/java/basilium/basiliumserver/BasiliumServerApplication.java
@@ -0,0 +1,13 @@
+package basilium.basiliumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiliumServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiliumServerApplication.class, args);
+	}
+
+}
