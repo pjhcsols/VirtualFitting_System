6919b672e09efc05688724977d52ceaf9a94484d
ryunaeun <naeun9822434@gmail.com>
2024-03-27 20:35:24 +0900

    [Feat]: 최상단 헤더 추가

---
+++ client_fe/src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderTop from '../components/Header_Top';
+
+function MainPage() {
+  return (
+    <div>
+        <HeaderTop />
+    </div>
+  );
+}
+
+export default MainPage;
