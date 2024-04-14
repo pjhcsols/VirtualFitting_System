a240041d29d5b11a10495b39571a0c5bfada7fd0
kimmokalover <kimmokalover@gmail.com>
2024-04-14 18:02:41 +0900

    feat : shopping cart 객체 생성

---
+++ basilium-server/src/main/java/basilium/basiliumserver/domain/shoppingCart/ShoppingCart.java
@@ -0,0 +1,43 @@
+package basilium.basiliumserver.domain.shoppingCart;
+
+import basilium.basiliumserver.domain.product.Product;
+import basilium.basiliumserver.domain.user.NormalUser;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "shopping_cart_sequence", sequenceName = "SHOPPING_CART_SEQUENCE", allocationSize = 1)
+    @Column(name = "shopping_cart_id", nullable = false, columnDefinition = "int")
+    Long shoppingCartId;
+
+    @ManyToOne
+    @JoinColumn(name = "userNumber")
+    NormalUser normalUser;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+
+
+    @Column(name = "amount")
+    Long amount;
+
+}
