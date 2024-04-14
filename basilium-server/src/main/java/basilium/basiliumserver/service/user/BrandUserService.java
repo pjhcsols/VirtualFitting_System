ca7af5c6d7df0011508de21074ca8c4f9765ac20
pjhcsols <pjhcsols@naver.com>
2024-04-14 23:29:48 +0900

    Feat: BrandUser 회원가입 로직 생성[박한솔]

--- basilium-server/src/main/java/basilium/basiliumserver/service/user/BrandUserService.java
+++ basilium-server/src/main/java/basilium/basiliumserver/service/user/BrandUserService.java
@@ -1,2 +1,85 @@
-package basilium.basiliumserver.service.user;public class BrandUserService {
+package basilium.basiliumserver.service.user;
+
+import basilium.basiliumserver.domain.user.BrandUser;
+import basilium.basiliumserver.domain.user.JoinStatus;
+import basilium.basiliumserver.domain.user.NormalUser;
+import basilium.basiliumserver.repository.user.BrandUserRepository;
+import basilium.basiliumserver.repository.user.NormalUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BrandUserService {
+
+    private final BrandUserRepository brandUserRepository;
+
+    @Autowired
+    public BrandUserService(BrandUserRepository brandUserRepository) {
+        this.brandUserRepository = brandUserRepository;
+    }
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private final Long expiredMs = 1000 * 60 * 60L;
+
+    public List<BrandUser> getAllBrandUsers() {
+        return brandUserRepository.getAllBrandUsers();
+    }
+
+    public void modify(BrandUser brandUser) {
+        brandUserRepository.modify(brandUser);
+    }
+
+    public BrandUser userInfoById(String userId) {
+        return brandUserRepository.findById(userId).orElse(null);
+    }
+
+    public JoinStatus join(BrandUser brandUser) {
+        try {
+            validateDuplicateMember(brandUser);
+            checkPasswordLength(brandUser);
+            checkStrongPassword(brandUser);
+            brandUserRepository.save(brandUser);
+            return JoinStatus.SUCCESS;
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+            return JoinStatus.DUPLICATE;
+        }
+    }
+
+    private void validateDuplicateMember(BrandUser brandUser) {
+        brandUserRepository.findById(brandUser.getId()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    private void checkPasswordLength(BrandUser brandUser) {
+        String password = brandUser.getPassword();
+        if (!(password.length() >= 8 && password.length() <= 16)) {
+            throw new IllegalStateException("비밀번호는 8 ~ 16자 사이여야 합니다.");
+        }
+    }
+
+    private void checkStrongPassword(BrandUser brandUser) {
+        String password = brandUser.getPassword();
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasSpecialChar = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLowerCase = true;
+            } else if ("!@#$%^&*()-_=+[]{}|;:'\",.<>/?".indexOf(c) != -1) {
+                hasSpecialChar = true;
+            }
+        }
+        if (!(hasUpperCase && hasLowerCase && hasSpecialChar)) {
+            throw new IllegalStateException("비밀번호는 영문 소문자, 대문자, 특수문자를 포함해야됩니다.");
+        }
+    }
 }
--- basilium-server/src/main/java/basilium/basiliumserver/service/user/NormalUserService.java
+++ basilium-server/src/main/java/basilium/basiliumserver/service/user/NormalUserService.java
@@ -41,6 +41,12 @@ public class NormalUserService {
     public List<NormalUser> getAllNormalUsers() {
         return normalUserRepository.getAllNormalUsers();
     }
+    public void modify(NormalUser normalUser) {
+        normalUserRepository.modify(normalUser);
+    }
+    public NormalUser userInfoById(String userId){
+        return normalUserRepository.findById(userId).get();
+    }
 //회원가입
     public JoinStatus join(NormalUser normalUser){
         try{
@@ -68,33 +74,6 @@ public class NormalUserService {
         normalUserRepository.save(normalUser);
         return JoinStatus.SUCCESS;
     }
-/*
-    public LoginStatus login(String userId, String userPassword){
-        Optional<NormalUser> tar = normalUserRepository.findById(userId);
-        if (tar.isEmpty() || !(tar.get().getPassword().equals(userPassword))){
-            return LoginStatus.FAIL;
-        }
-        return LoginStatus.SUCCESS;
-    }
-
-    public String afterSuccessLogin(String userId){
-        return JwtUtil.createJwt(userId, "normal", secretKey, expiredMs);
-    }
-    //로그아웃
-    public void logout(HttpServletRequest request) {
-        // 클라이언트 측에서 토큰을 삭제하거나 무효화하는 로직 추가
-        // 여기서는 클라이언트 측에서 토큰을 삭제하는 방법으로 가정
-
-        // 로그아웃한 토큰을 블랙리스트에 추가
-        String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String token = authorizationHeader.substring(7);
-            JwtUtil.blacklistToken(token);
-        }
-        // 다른 로그아웃 관련 로직 추가 가능
-    }
-
- */
 
     private void validateDuplicateMember(NormalUser normalUser) {
         normalUserRepository.findById(normalUser.getId()).ifPresent(m->{
@@ -121,6 +100,9 @@ public class NormalUserService {
         if (!(hasUpperCase && hasLowerCase && hasSpecialChar))throw new IllegalStateException("비밀번호는 영문 소문자, 대문자, 특수문자를 포함해야됩니다.");
     }
 
+
+
+
     public NormalUser getUserInfoByJWT(HttpServletRequest request){
         String authorizationHeader = request.getHeader("Authorization");
         String jwtToken = "";
@@ -133,9 +115,6 @@ public class NormalUserService {
         Optional<NormalUser> ret = normalUserRepository.findById(userId);
         return ret.orElse(null);
     }
-
-
-
 
 //리프레쉬
     public boolean validateRefreshToken(String refreshToken) {
@@ -156,13 +135,34 @@ public class NormalUserService {
         return null;
     }
 
-    public NormalUser userInfoById(String userId){
-        return normalUserRepository.findById(userId).get();
+
+
+    /*
+    public LoginStatus login(String userId, String userPassword){
+        Optional<NormalUser> tar = normalUserRepository.findById(userId);
+        if (tar.isEmpty() || !(tar.get().getPassword().equals(userPassword))){
+            return LoginStatus.FAIL;
+        }
+        return LoginStatus.SUCCESS;
     }
 
-    //수정
-    public void modify(NormalUser normalUser) {
-        normalUserRepository.modify(normalUser);
+    public String afterSuccessLogin(String userId){
+        return JwtUtil.createJwt(userId, "normal", secretKey, expiredMs);
     }
+    //로그아웃
+    public void logout(HttpServletRequest request) {
+        // 클라이언트 측에서 토큰을 삭제하거나 무효화하는 로직 추가
+        // 여기서는 클라이언트 측에서 토큰을 삭제하는 방법으로 가정
+
+        // 로그아웃한 토큰을 블랙리스트에 추가
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            JwtUtil.blacklistToken(token);
+        }
+        // 다른 로그아웃 관련 로직 추가 가능
+    }
+
+ */
 
 }
