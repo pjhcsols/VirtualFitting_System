99dfae91e80198780e52297b4ded3aba99b5b65e
kimmokalover <kimmokalover@gmail.com>
2024-04-14 18:12:27 +0900

    feat : like 객체 생성

---
+++ basilium-server/src/main/java/basilium/basiliumserver/domain/like/Like.java
@@ -0,0 +1,36 @@
+package basilium.basiliumserver.domain.like;
+
+import basilium.basiliumserver.domain.product.Product;
+import basilium.basiliumserver.domain.user.NormalUser;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "like_sequence", sequenceName = "LIKE_SEQUENCE", allocationSize = 1)
+    @Column(name = "like_id", nullable = false, columnDefinition = "int")
+    Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "userNumber")
+    NormalUser normalUser;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+}
