1a9643be5b179b2266b5cf6825abc6ba3d86bb87
Geonah Kim <kimgona1416@gmail.com>
2024-05-13 01:55:39 +0900

    Feat: 프론트엔드에서 보낸 url을 리스트로 받기[김건아]

--- AI/main.py
+++ AI/main.py
@@ -1,11 +1,74 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from typing import List
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/image/{userid}")
-def read_userid(user_id:int, user_iamge: str, cloth_image:str, type:Optional[int]=None):
-    return {"user_id":user_id, "user_image_url":user_iamge, "cloth_image_url":cloth_image, "cloth_type": type}
+# 모든 출처에서의 요청을 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처에서의 요청 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
-@app.post("result/{userid}")
-def save_result(user_id:int, image_location:str):
-    return{"user_id":user_id, "image_location":image_location}
+class RequestData(BaseModel):
+    userId: str
+    userImg: str
+    closeimage: str
+
+@app.post("/receive_data/")
+async def receive_data(request_data: List[str]):
+    try:
+        # 배열 형태의 데이터를 각각의 필드에 맞게 파싱
+        data = {
+            "userId": request_data[0],
+            "userImg": request_data[1],
+            "closeimage": request_data[2]
+        }
+        request_model = RequestData(**data)
+        return request_model.dict()
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"message": str(e)})
+
+
+# # Pydantic 모델 정의
+# class RequestData(BaseModel):
+#     userId: str
+#     responseBodyText: str
+#     productPhotoUrl: str
+    
+# @app.post("/")
+# async def process_data_root(data: RequestData):
+#     user_id = data.userId
+#     response_body_text = data.responseBodyText
+#     product_photo_url = data.productPhotoUrl
+    
+#     return {
+#         "userId": user_id,
+#         "responseBodyText": response_body_text,
+#         "productPhotoUrl": product_photo_url
+#     }
+
+# # # POST 요청을 처리하는 경로
+# # @app.post("/process_data/")
+# # async def process_data(data: RequestData):
+# #     user_id = data.userId
+# #     response_body_text = data.responseBodyText
+# #     product_photo_url = data.productPhotoUrl
+    
+# #     return {
+# #         "userId": user_id,
+# #         "responseBodyText": response_body_text,
+# #         "productPhotoUrl": product_photo_url
+# #     }
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+
