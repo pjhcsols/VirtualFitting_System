8fe9cf8ab0688d7b30a9a9f5e38e8a6e15f925f8
ryunaeun <naeun9822434@gmai.com>
2024-05-24 20:01:55 +0900

    Feat: 상품상세페이지 공유 버튼 활성화[류나은]

--- client_fe/src/pages/StorePage.jsx
+++ client_fe/src/pages/StorePage.jsx
@@ -5,19 +5,45 @@ import './StorePage.css';
 import heartIcon from '../assets/img/Heart.png';
 import { useNavigate } from 'react-router-dom';
 
-const Product = ({ product, onClick }) => (
-    <div className="store_product" onClick={onClick}>
-        <img className="product-image" src={product.productPhotoUrl[0]} alt="제품 사진" />
-        <div className="product-actions">
-            <img className='store_heart-icon' src={heartIcon} alt='heartIcon'/>
-            <button className="store_cart-icon">+cart</button>
+const Product = ({ product, onClick }) => {
+    /*
+    const handleHeartIconClick = async (e) => {
+        
+        e.stopPropagation(); // 상위로의 이벤트 전파 방지
+        try {
+            const response = await fetch(`http://218.233.221.41:8080/normalUser/shopping/list`, {
+                method: 'GET', // GET 메소드 사용
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer 여기에_토큰_값'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('서버로부터 응답을 받지 못했습니다.');
+            }
+            
+            const data = await response.json();
+            console.log(data); // 응답 로깅
+        } catch (error) {
+            console.error("하트 아이콘 클릭 처리 중 오류 발생:", error);
+        }
+    }; onClick={handleHeartIconClick}*/
+
+    return (
+        <div className="store_product" onClick={onClick}>
+            <img className="product-image" src={product.productPhotoUrl[0]} alt="제품 사진" />
+            <div className="product-actions">
+                <img className='store_heart-icon' src={heartIcon} alt='heartIcon' />
+                <button className="store_cart-icon">+cart</button>
+            </div>
+            <div className="icon_underline"></div>
+            <p className="product_title">{product.productName}</p>
+            <p className="product_price">{product.productPrice} won</p>
+            <p className="description">{product.productDesc}</p>
         </div>
-        <div className="icon_underline"></div>
-        <p className="product_title">{product.productName}</p>
-        <p className="product_price">{product.productPrice} won</p>
-        <p className="description">{product.productDesc}</p>
-    </div>
-);
+    );
+};
 
 function StorePage() {
     const [products, setProducts] = useState([]);
