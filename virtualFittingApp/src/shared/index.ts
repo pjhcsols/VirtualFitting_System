31afbee644efdc686af987573940631a250574c5
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-14 17:52:40 +0900

    ✨ Constants & add Shared Layer management file

---
+++ virtualFittingApp/src/shared/index.ts
@@ -0,0 +1,19 @@
+export { API_BASILIUM, API_BASILIUM_AI } from "./config/AxiosConfig";
+
+export { CheckBox } from "./components/checkbox/CheckBox";
+export { CategorySelectBar } from "./components/selectBar/CategorySelectBar";
+export { SearchBar } from "./components/searchBar/ui/SearchBar";
+export { Pagination } from "./components/pagination/ui/Pagination";
+export { AdminProductCard } from "./components/card/AdminProductCard";
+
+export { UserLayout } from "./layout/UserLayout";
+export { AdminLayout } from "./layout/AdminLayout";
+export { UserSignLayout } from "./layout/UserSignLayout";
+export { AdminSignLayout } from "./layout/AdminSignLayout";
+
+export {
+  type ColorType,
+  type MaterialType,
+  type SizeType,
+} from "./types/option.d";
+export { type USER_GRADE } from "./types/grade.d";
