5a844fdcc439f98db1c5dc931797c4eff56f5262
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-18 13:32:26 +0900

    🍱  add static assets

--- virtualFittingApp/src/shared/constants/index.ts
+++ virtualFittingApp/src/shared/constants/index.ts
@@ -2,8 +2,13 @@ import Basilium3DLogo from "/img/Basilium3DLogo.png";
 import SearchIcon from "/svg/SearchIcon.svg";
 import LeftArrow from "/svg/LeftArrow.svg";
 import RightArrow from "/svg/RightArrow.svg";
+import MyPageUser from "/img/MyPageUser.png";
+import WarningImg from "/img/Warning.png";
 
 export const IMG_BASILIUM_3D_LOGO = Basilium3DLogo;
+export const IMG_MYPAGE_USER = MyPageUser;
+export const IMG_WARNING = WarningImg;
+
 export const ICON_SEARCH = SearchIcon;
 export const ICON_LEFT_ARROW = LeftArrow;
 export const ICON_RIGHT_ARROW = RightArrow;
