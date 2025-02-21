2ef70d054b4cc9d0e14eb135780191555f6cf591
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-22 01:48:32 +0900

    ⚰️  Unused variable delete ([FE] Directory Structure 변경에 따른 error 해결 #73)

--- virtualFittingApp/src/pages/index.ts
+++ virtualFittingApp/src/pages/index.ts
@@ -1,23 +1,20 @@
 export { default as AboutPage } from "./about/ui/AboutPage";
 export { default as BrandPage } from "./brand/ui/BrandPage";
 
-// * Admin Page
-export { ProductRegisterationPage } from "./admin/ui/ProductRegisterationPage";
-
 // * New Admin Page
-export { AdminProductListPage } from "./admin/ui/AdminProductListPage";
-export { AdminCreateProductPage } from "./admin/ui/AdminCreateProductPage";
-export { AdminProductPage } from "./admin/ui/AdminProductPage";
+export { AdminProductListPage } from "@/pages/admin/ui/AdminProductListPage";
+export { AdminCreateProductPage } from "@/pages/admin/ui/AdminCreateProductPage";
+export { AdminProductPage } from "@/pages/admin/ui/AdminProductPage";
 
 // * SignUp Page
-export { UserSignUpPage } from "./signup/ui/UserSignUpPage";
-export { UserSignUpDetailPage } from "./signup/ui/UserSignUpDetailPage";
-export { BrandSignUpPage } from "./signup/ui/BrandSignUpPage";
+export { UserSignUpPage } from "@/pages/signup/ui/UserSignUpPage";
+export { UserSignUpDetailPage } from "@/pages/signup/ui/UserSignUpDetailPage";
+export { BrandSignUpPage } from "@/pages/signup/ui/BrandSignUpPage";
 
 // * New SignUP Page
 
 // * Store Page
-export { StorePage } from "./store/ui/StorePage";
-export { StoreDetailPage } from "./store/ui/StoreDetailPage";
+export { StorePage } from "@/pages/store/ui/StorePage";
+export { StoreDetailPage } from "@/pages/store/ui/StoreDetailPage";
 
 // * New Store Page
