7b323cea046a75a6878ad645352b6e652343a7b4
Seah Kim <seahkeem@gmail.com>
2025-02-27 15:16:09 +0900

    Refactor: Feature-Sliced Design 구조 변경 [VirtualFitting_System #73]

--- virtualFittingApp/src/pages/login/constants/index.ts
+++
@@ -1,9 +0,0 @@
-import img_background from '/img/background/background_login.png';
-import img_logo from '/img/logo/logo.png';
-import icon_firm from '/img/firm/firm_loginpage.png'
-import icon_user from '/img/user/user_signuppage.png'
-
-export const IMG_LOGO = img_logo;
-export const IMG_BACKGROUND = img_background;
-export const ICON_FIRM = icon_firm;
-export const ICON_USER = icon_user;
