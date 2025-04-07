fd263bed54428d66b80b4910e0e056a88a1d7d47
Seah Kim <seahkeem@gmail.com>
2025-04-07 21:50:48 +0900

    Refactor: Apply Responsive Breakpoints [#93]

--- virtualFittingApp/src/pages/store/ui/StorePage.tsx
+++ virtualFittingApp/src/pages/store/ui/StorePage.tsx
@@ -21,6 +21,12 @@ function StorePage() {
   );
 }
 
+export const xlDouble = 1536;
+export const xl = 1280;
+export const lg = 1024;
+export const md = 768;
+export const sm = 640;
+
 const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
@@ -39,24 +45,30 @@ const CarouselContainer = styled.div`
 const ProductGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-  padding: 5em 2em;
+  padding: 5em 1.5em;
   gap: 0;
   margin: 0 auto;
   max-width: 1000px;
 
-  @media (min-width: 600px) {
+  @media (min-width: ${sm}px) {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (min-width: 900px) {
+  @media (min-width: ${md}px) {
     grid-template-columns: repeat(3, 1fr);
   }
 
-  @media (min-width: 1200px) {
+  @media (min-width: ${xl}px) {
     grid-template-columns: repeat(4, 1fr);
     padding: 5em 10em;
   }
+
+  @media (min-width: ${xlDouble}px) {
+    max-width: 1400px;
+    padding: 5em 15em;
+  }
 `;
+
 
 
 export { StorePage };
