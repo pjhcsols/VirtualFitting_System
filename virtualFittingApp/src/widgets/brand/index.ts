732196881d0a86dff9d2edf974c05dcde3dea56f
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-04-13 21:10:02 +0900

    ✨ Feat Add Widget layer about brand page ([FE_SONU] 🚀 Brand Mypage 작업 #109)

---
+++ virtualFittingApp/src/widgets/brand/index.ts
@@ -0,0 +1,2 @@
+export * from "@/widgets/brand/ui/BrandInfoChanger";
+export * from "@/widgets/brand/ui/BrandButton";
--- virtualFittingApp/src/widgets/index.ts
+++ virtualFittingApp/src/widgets/index.ts
@@ -3,3 +3,5 @@ export * from "@/widgets/admin";
 export * from "@/widgets/store";
 
 export * from "@/widgets/main";
+
+export * from "@/widgets/brand";
