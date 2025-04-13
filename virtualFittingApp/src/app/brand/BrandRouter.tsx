e8fad8cc0ab79df3b1c55dd4b0e4dacef2af5b6f
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-04-13 21:03:18 +0900

    ✨ Feat: Router modified & cleaning ([FE_SONU] 🚀 Brand Mypage 작업 #109)

--- virtualFittingApp/src/app/brand/BrandRouter.tsx
+++ virtualFittingApp/src/app/brand/BrandRouter.tsx
@@ -2,13 +2,18 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { BrandPage } from "@/pages";
 
 import { AnimatePresence } from "motion/react";
+import { BrandProfile } from "@/pages/brand/ui/BrandProfile";
+import { BrandLayout } from "@/shared";
 
 function BrandRouter() {
   const location = useLocation();
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/brand" element={<BrandPage />} />
+        <Route path="brand" element={<BrandLayout />}>
+          <Route index element={<BrandPage />} />
+          <Route path="my" element={<BrandProfile />} />
+        </Route>
       </Routes>
     </AnimatePresence>
   );
