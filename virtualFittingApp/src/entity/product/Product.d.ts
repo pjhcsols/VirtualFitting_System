10addd0b04ef32429e4a17720398d04bdfc2a9eb
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-04-14 17:15:55 +0900

    Feat: Product Entity added ([FE_SONU] 🚀 Product Register Page #104)

---
+++ virtualFittingApp/src/entity/product/Product.d.ts
@@ -0,0 +1,9 @@
+export type ServerProductList = {
+  productId: number;
+  productName: string;
+  productPrice: number;
+  totalQuantity: number;
+  categoryName: string;
+  productColors: ["BLACK"];
+  productPhotoUrls: string[];
+};
