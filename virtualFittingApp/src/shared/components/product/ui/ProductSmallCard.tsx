a40e96c1a608d9aa1388a3563e9c4fbde157746b
Seah Kim <seahkeem@gmail.com>
2025-04-29 20:31:12 +0900

    Refactor : resizing Pruduct Detail Page

--- virtualFittingApp/src/shared/components/product/ui/ProductSmallCard.tsx
+++ virtualFittingApp/src/shared/components/product/ui/ProductSmallCard.tsx
@@ -1,23 +1,14 @@
 import styled from "styled-components";
 
-function ProductSmallCard({ src }: { src: string }) {
-  return (
-    <Wrapper>
-      <InnerClothes src={src} alt="small-card" />
-    </Wrapper>
-  );
+function ProductSmallCard() {
+  return <Wrapper />;
 }
 
 const Wrapper = styled.div`
-  width: 6vw;
-  height: 7.5vw;
-  overflow: hidden;
+  width: 72px;
+  height: 86px;
+  background-color: #e0e0e0; /* 연한 회색 */
   cursor: pointer;
-`;
-
-const InnerClothes = styled.img`
-  width: 100%;
-  height: 100%;
 `;
 
 export { ProductSmallCard };
