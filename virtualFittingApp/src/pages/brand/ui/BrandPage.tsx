3225e93bfa0caee928d30df7266178ecee6db12c
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-05-05 17:22:29 +0900

    Feat: Brand Page layer added ([FE] Define the allocation of roles for each work page #93)

--- virtualFittingApp/src/pages/brand/ui/BrandPage.tsx
+++ virtualFittingApp/src/pages/brand/ui/BrandPage.tsx
@@ -1,7 +1,151 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+
+import * as S from "@/pages/brand/ui/css/BrandPage.css";
+import { BasiliumNoAnimationLogo, BrandLandingHeader, PopLogo } from "@/shared";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+import { type LenisRef } from "lenis/react";
+import "lenis/dist/lenis.css";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function BrandPage() {
-  return <div>BrandPage</div>;
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        markers: true,
+      },
+    });
+
+    tl.fromTo(
+      ".carpet",
+      {
+        scrollTrigger: ".container",
+        y: 200,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        ease: "power4.out",
+      },
+    );
+    tl.fromTo(
+      [".fitting-title", ".fitting-text"],
+      {
+        scrollTrigger: ".fitting-sys",
+        start: "top end",
+        y: 50,
+        opacity: 0,
+      },
+      {
+        stagger: 0.25,
+        y: 0,
+        opacity: 1,
+        ease: "power3.out",
+      },
+    );
+    tl.fromTo(
+      [".interface-title", ".interface-text"],
+      {
+        scrollTrigger: ".interface-sys",
+        start: "top end",
+        y: 50,
+        opacity: 0,
+      },
+      {
+        stagger: 0.25,
+        y: 0,
+        opacity: 1,
+        ease: "power3.out",
+      },
+    );
+  }, []);
+
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
+  return (
+    <S.LenisWrapper
+      options={{ smoothWheel: true, autoRaf: false }}
+      ref={lenisRef}
+      root
+    >
+      <S.Wrapper>
+        <BrandLandingHeader />
+        <S.Container className="container">
+          {/* Hero Section */}
+          <S.HeroContainer>
+            <S.TitleContainer>
+              <S.Title>AI ShoppingMall Platform</S.Title>
+              <S.Title>with. BASILIUM</S.Title>
+            </S.TitleContainer>
+            <S.Carpet className="carpet">
+              <PopLogo />
+            </S.Carpet>
+          </S.HeroContainer>
+
+          {/* AI Fitting System Description Section */}
+          <S.DescSection className="fitting-sys">
+            <S.DescTitle className="fitting-title">
+              AI Fitting System
+            </S.DescTitle>
+            <S.ContentContainer>
+              <S.AIWearingContainer></S.AIWearingContainer>
+              <S.TextureContainer>
+                <S.DescText className="fitting-text">
+                  바실리움 입점 브랜드
+                </S.DescText>
+                <S.DescText className="fitting-text">
+                  상품들에 AI 시착 기능 구비
+                </S.DescText>
+              </S.TextureContainer>
+            </S.ContentContainer>
+          </S.DescSection>
+
+          {/* Brand User Interface Description Section */}
+          <S.InterfaceDescSection className="interface-sys">
+            <S.DescTitle className="interface-title">
+              Brand Interface
+            </S.DescTitle>
+            <S.ContentContainer>
+              <S.AIWearingContainer></S.AIWearingContainer>
+              <S.TextureContainer>
+                <S.DescText className="interface-text">
+                  간편한 상품 프로세스로
+                </S.DescText>
+                <S.DescText className="interface-text">
+                  필요한 정보들을 한번에 확인
+                </S.DescText>
+              </S.TextureContainer>
+            </S.ContentContainer>
+          </S.InterfaceDescSection>
+
+          {/* Basilium Description Section */}
+          <S.BasiliumDescSection>
+            <S.BasiliumLogoContainer>
+              <BasiliumNoAnimationLogo />
+            </S.BasiliumLogoContainer>
+            <S.BasiliumTextureContainer>
+              <S.BasiliumLogoText>Basilium</S.BasiliumLogoText>
+            </S.BasiliumTextureContainer>
+          </S.BasiliumDescSection>
+        </S.Container>
+      </S.Wrapper>
+    </S.LenisWrapper>
+  );
 }
 
 export { BrandPage };
