b1cb256cfb0e937cd8ac5da03f5eb45a161b258d
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-05-12 11:56:17 +0900

    Feat: Unused library deleted on Router file & Route modified ([FE_SONU] 🚀 Admin Login 페이지 & Admin Brand User 페이지 #113)

--- virtualFittingApp/src/app/admin/AdminRouter.tsx
+++ virtualFittingApp/src/app/admin/AdminRouter.tsx
@@ -2,20 +2,17 @@ import { Route, Routes, useLocation } from "react-router-dom";
 
 import { AdminLayout } from "@/shared";
 
-import { AnimatePresence } from "motion/react";
 import { AdminBrandControllPanel, AdminLogin } from "@/pages";
 
 function AdminRouter() {
   const location = useLocation();
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="admin" element={<AdminLayout />}>
-          <Route index element={<AdminLogin />}></Route>
-          <Route path=":id" element={<AdminBrandControllPanel />}></Route>
-        </Route>
-      </Routes>
-    </AnimatePresence>
+    <Routes location={location} key={location.pathname}>
+      <Route path="admin/login" element={<AdminLogin />} />
+      <Route path="admin" element={<AdminLayout />}>
+        <Route path=":id" element={<AdminBrandControllPanel />} />
+      </Route>
+    </Routes>
   );
 }
 
