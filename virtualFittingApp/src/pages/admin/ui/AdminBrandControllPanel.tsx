956351fa6bcc5d1a4f86410213c47782719f698f
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-05-12 11:55:34 +0900

    Feat: Admin Brand User List Page layer added ([FE_SONU] 🚀 Admin Login 페이지 & Admin Brand User 페이지 #113

--- virtualFittingApp/src/pages/admin/ui/AdminBrandControllPanel.tsx
+++ virtualFittingApp/src/pages/admin/ui/AdminBrandControllPanel.tsx
@@ -1,7 +1,15 @@
 import * as S from "@/pages/admin/ui/css/AdminBrandControllPanel.css";
+import { AdminBrandUserList } from "@/widgets";
 
 function AdminBrandControllPanel() {
-  return <S.Wrapper></S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.TitleContainer>
+        <S.Title>Brand User List</S.Title>
+      </S.TitleContainer>
+      <AdminBrandUserList />
+    </S.Wrapper>
+  );
 }
 
 export { AdminBrandControllPanel };
--- virtualFittingApp/src/pages/admin/ui/css/AdminBrandControllPanel.css.ts
+++ virtualFittingApp/src/pages/admin/ui/css/AdminBrandControllPanel.css.ts
@@ -3,6 +3,20 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 100%;
   display: flex;
+  flex-flow: column wrap;
   justify-content: center;
   align-items: center;
+`;
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-family: "Prata-Regular";
+  font-size: 1.5rem;
+  color: black;
 `;
