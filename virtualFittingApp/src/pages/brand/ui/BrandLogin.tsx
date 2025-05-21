f53c566ae5be3fabe623e5c375a2514e86dc1ff0
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-05-21 17:26:26 +0900

    Feat: Login Router modified for login & Brand Login page modified ([FE_SONU] 🚀 Admin Login 페이지 & Admin Brand User 페이지 #113)

--- virtualFittingApp/src/pages/brand/ui/BrandLogin.tsx
+++ virtualFittingApp/src/pages/brand/ui/BrandLogin.tsx
@@ -1,7 +1,67 @@
 import * as S from "@/pages/brand/ui/css/BrandLogin.css";
+import { Basilium3DModelLogin, LoginInput } from "@/shared";
+import { ChangeEvent, useRef, useState } from "react";
+import { type BrandSigninUserType } from "@/pages/brand/types/brandUser";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
 
 function BrandLogin() {
-  return <div>BrandLogin</div>;
+  const [brandUserInfo, setBrandUserInfo] = useState<BrandSigninUserType>({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "email" || name === "password") {
+      setBrandUserInfo({
+        ...brandUserInfo,
+        [name]: value,
+      });
+    }
+  };
+
+  const RotatingStars = () => {
+    const stars = useRef(null);
+
+    useFrame(() => {
+      if (stars.current) {
+        stars.current.rotation.x = stars.current.rotation.y += 0.00015;
+      }
+    });
+
+    return <Stars ref={stars} />;
+  };
+
+  return (
+    <S.Wrapper>
+      <S.StarWrapper>
+        <Canvas>
+          <OrbitControls />
+          <RotatingStars />
+        </Canvas>
+      </S.StarWrapper>
+      <S.LogoWrapper>
+        <Basilium3DModelLogin />
+      </S.LogoWrapper>
+      <S.InfoWrapper>
+        <LoginInput
+          value={brandUserInfo.email}
+          name="email"
+          type={true}
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <LoginInput
+          value={brandUserInfo.password}
+          name="password"
+          type={false}
+          placeholder="password"
+          onChange={onChange}
+        />
+      </S.InfoWrapper>
+    </S.Wrapper>
+  );
 }
 
 export { BrandLogin };
