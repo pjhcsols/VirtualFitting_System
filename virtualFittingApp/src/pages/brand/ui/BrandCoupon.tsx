0f9f99f53e5a327961c0e448636e2ea0075211a9
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-08 20:22:06 +0900

    Feat: Coupon 페이지 디자인 (#141) (#113)

--- virtualFittingApp/src/pages/brand/ui/BrandCoupon.tsx
+++ virtualFittingApp/src/pages/brand/ui/BrandCoupon.tsx
@@ -1,5 +1,5 @@
 import * as S from "@/pages/brand/ui/css/BrandCoupon.css";
-import { BrandCouponCard } from "@/shared";
+import { BrandCouponCard, ExpiredCouponIcon } from "@/shared";
 
 function BrandCoupon() {
   return (
@@ -8,9 +8,19 @@ function BrandCoupon() {
         <S.CouponTitle>쿠폰</S.CouponTitle>
       </S.CouponBannerWrapper>
       <BrandCouponCard couponCount={0} />
+      <S.CouponExpiredCountPanel>
+        <S.ExpiredCouponPanel>
+          <ExpiredCouponIcon width="24px" height="24px" fill="black" />
+        </S.ExpiredCouponPanel>
+        <S.ExpiredText>발행 종료된 쿠폰</S.ExpiredText>
+        <S.ExpiredTitle>0</S.ExpiredTitle>
+      </S.CouponExpiredCountPanel>
       <S.CouponList>
         <S.CouponSmallTitle>쿠폰 발행 리스트</S.CouponSmallTitle>
       </S.CouponList>
+      <S.CouponCreatePanel>
+        <S.CouponSmallTitle>쿠폰 추가 버튼</S.CouponSmallTitle>
+      </S.CouponCreatePanel>
     </S.Wrapper>
   );
 }
--- virtualFittingApp/src/pages/brand/ui/css/BrandCoupon.css.ts
+++ virtualFittingApp/src/pages/brand/ui/css/BrandCoupon.css.ts
@@ -13,7 +13,7 @@ export const Wrapper = styled.div`
 export const CouponBannerWrapper = styled.div`
   box-sizing: border-box;
   padding: 2rem;
-  min-width: 50rem;
+  min-width: calc(40rem + 16px);
   min-height: 10rem;
   border-radius: 8px;
   display: flex;
@@ -21,6 +21,10 @@ export const CouponBannerWrapper = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   background-color: white;
+  transition: 0.15s all ease;
+  &:hover {
+    background-color: #efefef;
+  }
 `;
 
 export const CouponTitle = styled.h1`
@@ -46,4 +50,65 @@ export const CouponList = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   background-color: white;
+  transition: 0.15s all ease;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+export const CouponCreatePanel = styled.div`
+  box-sizing: border-box;
+  padding: 2rem;
+  min-width: 20rem;
+  min-height: 20rem;
+  border-radius: 8px;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  background-color: white;
+  transition: 0.15s all ease;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+export const CouponExpiredCountPanel = styled.div`
+  box-sizing: border-box;
+  padding: 1.5rem;
+  min-width: 10rem;
+  min-height: 10rem;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  border-radius: 12px;
+  background-color: white;
+  transition: 0.15s all ease;
+  flex-shrink: 1;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+export const ExpiredCouponPanel = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  background-color: rgb(216, 216, 185);
+`;
+
+export const ExpiredTitle = styled.h2`
+  font-size: 0.9rem;
+  font-weight: 700;
+  color: black;
+`;
+
+export const ExpiredText = styled.span`
+  font-size: 0.75rem;
+  font-weight: 500;
+  color: #a0a0a0;
 `;
