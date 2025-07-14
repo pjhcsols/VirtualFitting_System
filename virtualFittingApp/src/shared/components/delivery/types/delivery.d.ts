87b5d101b58be2b99830ee75e11fc4b9917ec73c
Seah Kim <seahkeem@gmail.com>
2025-07-14 18:26:48 +0900

    Refactor: clean up old delivery-related components and code [VirtualFitting_System #141]

--- virtualFittingApp/src/shared/components/delivery/types/delivery.d.ts
+++
@@ -1,9 +0,0 @@
-export interface DeliveryInfo {
-    defaultDeliveryAddress: string;
-    firstDeliveryAddress: string;
-    secondDeliveryAddress: string;
-}
-
-export interface UserData {
-    deliveryInfo: DeliveryInfo;
-}
