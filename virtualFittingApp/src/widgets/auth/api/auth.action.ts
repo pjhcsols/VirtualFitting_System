7e4e338ed344dfc2ea79fa2081524a313336282e
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-19 23:02:12 +0900

    Fix: Login.html 로 돌아가는 현상 해결 (#141)

--- virtualFittingApp/src/widgets/auth/api/auth.action.ts
+++ virtualFittingApp/src/widgets/auth/api/auth.action.ts
@@ -1,5 +1,6 @@
 import { API_BASILIUM } from "@/shared";
 import type { LoginRequestDto } from "@/widgets/auth/types/login";
+import axios from "axios";
 import Cookies from "js-cookie";
 
 export const Logout = async () => {
@@ -15,7 +16,9 @@ export const Logout = async () => {
 
 export const userLogin = async (userInfo: LoginRequestDto) => {
   try {
-    const res = await API_BASILIUM.post("/b1/users/login", userInfo);
+    const BASILIUM_URL = import.meta.env
+      .VITE_APPLICATION_BASILIUM_SERVER as string;
+    const res = await axios.post(`${BASILIUM_URL}/b1/users/login`, userInfo);
     console.log(res);
     if (res.status === 200) {
       console.log(res.data);
