d48b145ac14b757aad9bcaf062bdd7e491754095
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-07-21 19:30:13 +0900

    Feat: 회원가입 성공/실패 페이지 생성 및 Normal User 회원가입 페이지 생성 (#141)

---
+++ virtualFittingApp/src/pages/auth/constants/index.ts
@@ -0,0 +1,5 @@
+export const TitleArray = [
+  "Basilium 이용약관 동의",
+  "회원가입 정보 기입",
+  "추가정보 기입",
+];
--- virtualFittingApp/src/pages/auth/index.ts
+++ virtualFittingApp/src/pages/auth/index.ts
@@ -2,3 +2,5 @@ export { Loginpage } from "@/pages/auth/ui/Loginpage";
 export { SignupPage } from "@/pages/auth/ui/SignupPage";
 export { BrandSignUpPage } from "@/pages/auth/ui/BrandSignUpPage";
 export { NormalSignUpPage } from "@/pages/auth/ui/NormalSignUpPage";
+export { SuccessSignUpPage } from "@/pages/auth/ui/SuccessSignUpPage";
+export { FailedPage } from "@/pages/auth/ui/FailedPage";
