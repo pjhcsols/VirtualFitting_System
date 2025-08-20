5aef7eabaa961991b7cc06abffd55aef7abb1296
Seah Kim <seahkeem@gmail.com>
2025-08-20 15:26:04 +0900

    refactor: payment result layout

--- virtualFittingApp/src/app/payment/PaymentRouter.tsx
+++ virtualFittingApp/src/app/payment/PaymentRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { CheckoutPage } from "@/pages";
 import { SuccessPage, FailPage } from "@/features";
 import { AnimatePresence } from "motion/react";
+import { PaymentLayout } from "@/shared";
 
 export function PaymentRouter() {
   const location = useLocation();
@@ -9,9 +10,10 @@ export function PaymentRouter() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        {/* <Route path="/payment/checkout" element={<CheckoutPage />} /> */}
-        <Route path="/payment-success" element={<SuccessPage />} />
-        <Route path="/payment-fail" element={<FailPage />} />
+        <Route path="payment" element={<PaymentLayout />}>
+          <Route path="success" element={<SuccessPage />} />
+          <Route path="fail" element={<FailPage />} />
+        </Route>
       </Routes>
     </AnimatePresence>
   );
--- virtualFittingApp/src/shared/layout/index.ts
+++ virtualFittingApp/src/shared/layout/index.ts
@@ -12,6 +12,8 @@ export * from "@/shared/layout/shopping-cart/ShoppingCartLayout";
 
 export * from "@/shared/layout/brand/BrandLayout";
 
+export * from "@/shared/layout/payment/PaymentLayout";
+
 export { LoginLayout } from "@/shared/layout/login/LoginLayout";
 
 export * from "@/shared/layout/order-list/OrderListLayout";
