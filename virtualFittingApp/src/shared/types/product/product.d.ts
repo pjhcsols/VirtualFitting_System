f319ae183b7f452348bc3aca0c61fc3de22c8f9d
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-08-27 20:57:28 +0900

    Feat: 제품 엔티티 수정

--- virtualFittingApp/src/shared/types/product/product.d.ts
+++ virtualFittingApp/src/shared/types/product/product.d.ts
@@ -1,4 +1,6 @@
-export type ProductServerResponseType = {
+// * DTO
+// * ============================================================
+export type ServerProductDto = {
   productId: number;
   productName: string;
   productPrice: number;
@@ -9,18 +11,23 @@ export type ProductServerResponseType = {
 };
 
 export type ClientProductDto = {
-  productName: string;
-  productDesc: string;
-  productPrice: number | string;
-  productMaterial: Material;
-  productColor: Color;
-  productMainPhotos: File[] | null;
-  productSubPhotos: File[] | null;
-  productSizeTable: SizeTable[] | "F";
-  productOptions: productOptionType[];
-  productColorOptions: productColorOptionType[];
+  productId: number;
+  status: ProductStatus;
   productCategory: ProductCategory;
+  productName: string;
+  productPrice: number;
+  productMaterial: Material[];
+  productDesc: string;
+  totalQuantity: number;
+  productOptions: ProductOptionType[];
+  productSizeOptions: ProductSizeOptionType[];
+  productColorOptions: productColorOptionType[];
+  version: 0;
 };
+
+// * Static Value
+// * ============================================================
+type ProductStatus = "ON SALE" | "EXHIBITION_STOPPED";
 
 export type Color =
   | "BLACK"
@@ -36,7 +43,7 @@ export type Size = "XX" | "S" | "M" | "L" | "XL";
 
 export type Material = "COTTON" | "POLYESTER" | "WOOL" | "FABRIC" | "SILK";
 
-export type SizeTable = {
+export type ProductSizeTableType = {
   productSize: Size;
   productTotalLength: number | string;
   productChest: number | string;
@@ -44,23 +51,59 @@ export type SizeTable = {
   productArm: number | string;
 };
 
+// * Request DTO
+// * ============================================================
 export type ProductCategory = {
   categoryId: number;
   categoryName: string;
 };
 
-type productOptionType = {
+// * Product Option
+// * ============================================================
+type ProductOptionIdType = {
+  productId: number;
   productSize: Size;
   productColor: Color;
+};
+
+type ProductOptionType = {
+  id: ProductOptionIdType;
+  product: string;
+  // 해당 수량으로 TotalQuantity 로 들어간다.
   optionQuantity: number;
 };
 
-type productColorOptionType = {
+// * Product Size Table
+// * ============================================================
+type ProductSizeOptionIdType = {
+  productId: number;
+  productSize: Size;
+};
+
+export type ProductSizeOptionType = {
+  id: ProductSizeIdType;
+  product: String;
+  totalLength: number;
+  chest: number;
+  shoulder: number;
+  arm: number;
+};
+
+// * Product Color Option
+// * ============================================================
+type ProductColorOptionIdType = {
+  productId: number;
   productColor: Color;
+};
+
+type productColorOptionType = {
+  id: ProductColorOptionIdType;
+  product: String;
   productPhotoUrls: string[];
   productSubPhotoUrls: string[];
 };
 
+// * ============================================================
 export type BrandProductSimpleType = {
   productId: number;
   photoUrl?: string;
