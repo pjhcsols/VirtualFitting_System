b23bbf3ffed5c317d0c003cdb21462c9bb39cdbf
Seah Kim <seahkeem@gmail.com>
2025-09-07 18:31:55 +0900

    delete: 폴더 이름 변경

--- virtualFittingApp/src/shared/components/shopping-cart/types/shoppingCartProps.d.ts
+++
@@ -1,5 +0,0 @@
-import { Product } from './product';
-
-export interface ShoppingCartProps {
-  shoppingData: Product[];
-}
--- virtualFittingApp/src/widgets/shopping-cart/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "@/widgets/shopping-cart/ui/CartPurchaseSummary";
-export * from "@/widgets/shopping-cart/ui/CartItemList";
-export * from "@/widgets/shopping-cart/constants/index";
