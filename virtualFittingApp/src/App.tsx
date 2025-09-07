3db96a1cd17703e0332fb110e02a4d9f02c1ef8e
Seah Kim <seahkeem@gmail.com>
2025-09-07 18:14:34 +0900

    feat: PG 성공 후 서버에 승인 처리 요청

--- virtualFittingApp/src/App.tsx
+++ virtualFittingApp/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { MyPage } from "@/pages/my";
@@ -9,7 +9,7 @@ import { MyLike } from "@/pages/my/ui/MyLike";
 import { MyOrderListDetail } from "@/pages/my/ui/MyOrderListDetail";
 import { MyReview } from "@/pages/my";
 import { StyleReview } from "@/pages/my";
-
+import { PaymentFailPage, PaymentSuccessPage } from "@/features";
 import {
   MainPage,
   AdminLogin,
@@ -46,72 +46,79 @@ import {
   StoreLayout,
   OrderListLayout,
   MyPageLayout,
+  PaymentLayout,
 } from "@/shared";
 import { SignUpLayout } from "@/shared/layout/auth";
 import { BrandProductLayout } from "@/shared/layout/brand/BrandProductLayout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<MainLayout />}>
-          <Route index element={<MainPage />} />
-        </Route>
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="login">
-          <Route index element={<Loginpage />} />
-          <Route path="admin" element={<AdminLogin />} />
-        </Route>
-        <Route path="signup" element={<SignUpLayout />}>
-          <Route index element={<NormalSignUpPage />} />
-          <Route path="brand" element={<BrandSignUpPage />} />
-          <Route path="success" element={<SuccessSignUpPage />} />
-          <Route path="failed" element={<FailedPage />} />
-        </Route>
-        <Route path="admin" element={<AdminLayout />}>
-          <Route index element={<AdminHome />} />
-          <Route path="banner" element={<AdminBanner />} />
-          <Route path="brand" element={<AdminBrand />} />
-          <Route path="user" element={<AdminUser />} />
-          <Route path="product" element={<AdminProduct />} />
-          <Route path="*" element={<BasiliumRedirect />} />
-        </Route>
-        <Route path="brand">
-          <Route index element={<BrandPage />} />
-          <Route element={<BrandLayout />}>
-            <Route path="dashboard" index element={<BrandDashboard />} />
-            <Route path="product" element={<BrandProductLayout />}>
-              <Route path="list" index element={<BrandProductListPage />} />
-              <Route path="create" element={<BrandProductCreate />} />
-              <Route path="coupon" element={<BrandCoupon />} />
-            </Route>
-            <Route path="payment" element={<BrandPaymentPage />} />
-            <Route path="my" element={<BrandProfile />} />
-            <Route path="analytics" element={<BrandAnalytics />} />
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<MainLayout />}>
+            <Route index element={<MainPage />} />
+          </Route>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="login">
+            <Route index element={<Loginpage />} />
+            <Route path="admin" element={<AdminLogin />} />
+          </Route>
+          <Route path="signup" element={<SignUpLayout />}>
+            <Route index element={<NormalSignUpPage />} />
+            <Route path="brand" element={<BrandSignUpPage />} />
+            <Route path="success" element={<SuccessSignUpPage />} />
+            <Route path="failed" element={<FailedPage />} />
+          </Route>
+          <Route path="admin" element={<AdminLayout />}>
+            <Route index element={<AdminHome />} />
+            <Route path="banner" element={<AdminBanner />} />
+            <Route path="brand" element={<AdminBrand />} />
+            <Route path="user" element={<AdminUser />} />
+            <Route path="product" element={<AdminProduct />} />
             <Route path="*" element={<BasiliumRedirect />} />
           </Route>
-        </Route>
-        <Route path="/shopping-cart" element={<ShoppingCartLayout />}>
-          <Route index element={<ShoppingCartPage />} />
-        </Route>
-        <Route path="store" element={<StoreLayout />}>
-          <Route index element={<StorePage />} />
-          <Route path=":id" element={<StoreDetailPage />} />
-        </Route>
-        <Route path="/order" element={<OrderListLayout />}>
-          <Route index element={<OrderListPage />} />
-        </Route>
-        <Route path="/mypage" element={<MyPageLayout />}>
-          <Route index element={<MyPage />} /> {/* /mypage */}
-          <Route path="order" element={<MyOrderList />} /> {/* /mypage/order */}
-          <Route path="order/:id" element={<MyOrderListDetail />} />
-          <Route path="cancel" element={<MyCancel />} />
-          <Route path="like" element={<MyLike />} />
-          <Route path="review" element={<MyReview />} />
-          <Route path="review/:id" element={<StyleReview />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="brand">
+            <Route index element={<BrandPage />} />
+            <Route element={<BrandLayout />}>
+              <Route path="dashboard" index element={<BrandDashboard />} />
+              <Route path="product" element={<BrandProductLayout />}>
+                <Route path="list" index element={<BrandProductListPage />} />
+                <Route path="create" element={<BrandProductCreate />} />
+                <Route path="coupon" element={<BrandCoupon />} />
+              </Route>
+              <Route path="payment" element={<BrandPaymentPage />} />
+              <Route path="my" element={<BrandProfile />} />
+              <Route path="analytics" element={<BrandAnalytics />} />
+              <Route path="*" element={<BasiliumRedirect />} />
+            </Route>
+          </Route>
+          <Route path="/cart" element={<ShoppingCartLayout />}>
+            <Route index element={<ShoppingCartPage />} />
+          </Route>
+          <Route path="store" element={<StoreLayout />}>
+            <Route index element={<StorePage />} />
+            <Route path=":id" element={<StoreDetailPage />} />
+          </Route>
+          <Route path="/payment" element={<PaymentLayout />}>
+            <Route path="success" element={<PaymentSuccessPage />} />
+            <Route path="fail" element={<PaymentFailPage />} />
+          </Route>
+          <Route path="/order" element={<OrderListLayout />}>
+            <Route index element={<OrderListPage />} />
+          </Route>
+          <Route path="/mypage" element={<MyPageLayout />}>
+            <Route index element={<MyPage />} /> {/* /mypage */}
+            <Route path="order" element={<MyOrderList />} /> {/* /mypage/order */}
+            <Route path="order/:id" element={<MyOrderListDetail />} />
+            <Route path="cancel" element={<MyCancel />} />
+            <Route path="like" element={<MyLike />} />
+            <Route path="review" element={<MyReview />} />
+            <Route path="review/:id" element={<StyleReview />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
--- virtualFittingApp/src/features/payment/ui/PaymentFailPage.tsx
+++ virtualFittingApp/src/features/payment/ui/PaymentFailPage.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from "react-router-dom"
 
-export function FailPage() {
+export function PaymentFailPage() {
   const [searchParams] = useSearchParams();
 
   return (
