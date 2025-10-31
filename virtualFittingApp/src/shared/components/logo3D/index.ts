45576af88db808ae3341d9eb09a4faf60d119d43
Seah Kim <seahkeem@gmail.com>
2025-10-31 19:31:50 +0900

    refactor: hero section

--- virtualFittingApp/src/shared/components/logo3D/index.ts
+++ virtualFittingApp/src/shared/components/logo3D/index.ts
@@ -1,4 +1,5 @@
 export * from "@/shared/components/logo3D/ui/Basilium3DLogo";
+export * from "./ui/Basilium3DLogoMain";
 export * from "@/shared/components/logo3D/ui/PopLogo";
 export * from "@/shared/components/logo3D/ui/BasiliumNoAnimationLogo ";
 export * from "@/shared/components/logo3D/ui/BasiliumSVGLogo";
