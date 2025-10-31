22631938ec1f11f7a739249d69d4d3078d924871
Seah Kim <seahkeem@gmail.com>
2025-10-31 19:26:29 +0900

    chore: 쿼리 클라이언트 추가

--- virtualFittingApp/src/main.tsx
+++ virtualFittingApp/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+import { QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <QueryClientProvider client={queryClient}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
