2a9169668545bbad80a641cb129efeba2210c56d
Seah Kim <seahkeem@gmail.com>
2025-11-02 06:38:21 +0900

    feat: 이미지 우클릭 미적용

--- virtualFittingApp/src/App.tsx
+++ virtualFittingApp/src/App.tsx
@@ -6,4 +6,13 @@ const App = () => {
   return <Routing />;
 };
 
+document.addEventListener('contextmenu', (event) => {
+  const target = event.target as HTMLElement;
+
+  // <img> 태그에 대해서만 기본 동작을 막습니다.
+  if (target.tagName === 'IMG') {
+    event.preventDefault();
+  }
+});
+
 export default withProviders(App);
