9c2c2bb47aae9bc45d93ada0b9cd7e4b601d493c
Seah Kim <seahkeem@gmail.com>
2025-11-02 13:39:13 +0900

    style: 상품 상세페이지 탭바 스타일 수정

--- virtualFittingApp/src/pages/product/detail/ui/product-detail-page.tsx
+++ virtualFittingApp/src/pages/product/detail/ui/product-detail-page.tsx
@@ -128,17 +128,53 @@ const TabMenu = styled.div`
   margin-top: 64px;
   flex-wrap: nowrap;
   gap: 64px;
+
 `;
 
 const TabButton = styled.button<{ $active: boolean }>`
-  padding: 8px 32px;
   border: none;
-  background: transparent;
-  color: ${({ $active }) => ($active ? "#c3c3c3ff" : "#ffffffff")};
-  font-size: 15px;
+  font-size: 16px;
   font-weight: ${({ $active }) => ($active ? 700 : 500)};
   cursor: pointer;
   white-space: nowrap;
+  background: none;
+  padding: 4px 2px;
+  position: relative;
+  cursor: pointer;
+
+  text-decoration: none; 
+  
+  color: rgba(255, 255, 255, 0.85);
+  transition: color 0.3s ease;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: rgb(255, 255, 255);
+    transform: scaleX(0);
+    transform-origin: center;
+    transition: transform 0.3s ease-out;
+  }
+
+  &:hover {
+    color: rgb(255, 255, 255);
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
+
+  &.active {
+    color: rgb(255, 255, 255);
+  }
+
+  &.active::after {
+    transform: scaleX(1);
+  }
 
   @media (max-width: ${BREAKPOINTS.md}px) {
     padding: 8px 8px;
