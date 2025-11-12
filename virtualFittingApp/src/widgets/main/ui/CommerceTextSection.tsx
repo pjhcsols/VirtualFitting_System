c1287195a5967a093b84e802df06e1ede8add6f4
Seah Kim <seahkeem@gmail.com>
2025-11-12 20:12:26 +0900

    style: 스크롤 텍스트 위치 수정

--- virtualFittingApp/src/widgets/main/ui/CommerceTextSection.tsx
+++ virtualFittingApp/src/widgets/main/ui/CommerceTextSection.tsx
@@ -110,7 +110,7 @@ const ScreenText = styled.div`
   letter-spacing: -1px;
 
   position: absolute;
-  top: 2%;
+  top: 10vh;
   left: 50%;
   transform: translate(-50%, -50%);
   
@@ -140,7 +140,6 @@ const SubText = styled.div`
   color: #fff;
   text-align:right;
 `;
-
 
 const ImageBox = styled.div`
   width: 40vw;
