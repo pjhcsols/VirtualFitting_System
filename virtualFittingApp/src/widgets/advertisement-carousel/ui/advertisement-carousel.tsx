64239374ce42cb88fc6179d5b6325eb039db5dfe
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-11-13 10:14:42 +0900

    Feat: 빌드에러 수정

--- virtualFittingApp/src/widgets/advertisement-carousel/ui/advertisement-carousel.tsx
+++ virtualFittingApp/src/widgets/advertisement-carousel/ui/advertisement-carousel.tsx
@@ -46,16 +46,27 @@ function AdvertisementCarousel() {
     setIsPaused((prev) => !prev);
   };
 
-  const cacheKey = banners.map(b => b.url).join('');
+  const cacheKey = banners.map((b) => b.url).join("");
   let currentBannerUrl = banners[currentIndex]?.url;
-  const uniqueUrl = currentBannerUrl 
-    ? `${currentBannerUrl}?v=${encodeURIComponent(cacheKey)}` 
-    : '';
-    
+  if (currentBannerUrl) {
+    if (currentBannerUrl.startsWith("http:")) {
+      currentBannerUrl = currentBannerUrl.replace("http:", "");
+    } else if (currentBannerUrl.startsWith("https:")) {
+      currentBannerUrl = currentBannerUrl.replace("https:", "");
+    }
+
+    if (!currentBannerUrl.startsWith("//")) {
+      currentBannerUrl = `//${currentBannerUrl.replace(/^\/\//, "")}`;
+    }
+  }
+  const uniqueUrl = currentBannerUrl
+    ? `${currentBannerUrl}?v=${encodeURIComponent(cacheKey)}`
+    : "";
+
   return (
     <Wrapper>
       <Image
-        src={`https://api.basilium.ai.kr${finalBannerUrl}`}
+        src={`https://api.basilium.ai.kr${uniqueUrl}`}
         alt={`Banner ${currentIndex + 1}`}
       />
 
